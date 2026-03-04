00fcd57064250886e025e03e650cb2527fbf0121
louisgreenwood04 <l.greenwood-23@student.lboro.ac.uk>
2026-03-04 19:49:07 +0000

    additions to make USS sensor code work

--- arduino/distanceValidation_USS/distanceValidation_USS.ino
+++ arduino/distanceValidation_USS/distanceValidation_USS.ino
@@ -7,15 +7,15 @@ const int trigPin = 9;
 const int echoPin = 10;
 
 // Tunables
-const int SWEEP_PWM = 90;                  // adjust 120–200
-const unsigned long SWEEP_MS_180 = 5500;    // estimated time of spinning
-const unsigned long SWEEP_MS_MAX = 6000;    //prevents multiple spinning 
+const int SWEEP_PWM = 90;
+const unsigned long SWEEP_MS_180 = 5500;
 const unsigned long SAMPLE_PERIOD_MS = 50;
 
-const float DEG_PER_MS = 180.0f / (float)SWEEP_MS_180;
-
 void setup() {
-  for (int i = 5; i <= 8; i++) pinMode(i, OUTPUT);
+  pinMode(E1, OUTPUT);
+  pinMode(E2, OUTPUT);
+  pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -24,57 +24,50 @@ void setup() {
   Serial.begin(9600);
   delay(200);
 
-  Serial.println("t_ms,angle_deg_est,distance_cm");
+  // CSV header
+  Serial.println("time_ms,distance_cm");
 }
 
 void loop() {
   smoothSweep180WithLogging();
-  stopCoast(); 
-  while (true) { }
+  stopCoast();
+
+  while(true) {} // run once
 }
 
 void smoothSweep180WithLogging() {
+
   unsigned long start = millis();
   unsigned long nextSample = start;
 
   setLeftTurnDirection();
 
-  // Start move
   analogWrite(E1, SWEEP_PWM);
   analogWrite(E2, SWEEP_PWM);
 
   while (true) {
+
     unsigned long now = millis();
     unsigned long elapsed = now - start;
 
-    // Stop 
     if (elapsed >= SWEEP_MS_180) break;
-    if (elapsed >= SWEEP_MS_MAX) break;
 
-    // colelct samp
     if (now >= nextSample) {
-      long d = getDistanceCm();
-      float angle = elapsed * DEG_PER_MS;
+
+      long distance = getDistanceCm();
 
       Serial.print(elapsed);
       Serial.print(",");
-      Serial.print(angle, 2);
-      Serial.print(",");
-
-      if (d == 0) Serial.println("0");
-      else Serial.println(d);
+      Serial.println(distance);
 
       nextSample += SAMPLE_PERIOD_MS;
     }
   }
 
-  // overshoot prevents 
   stopBrake();
-  Serial.println("DONE");
 }
 
 void setLeftTurnDirection() {
- 
   digitalWrite(M1, HIGH);
   digitalWrite(M2, LOW);
 }
@@ -85,25 +78,24 @@ void stopCoast() {
 }
 
 void stopBrake() {
-  analogWrite(E1, 120);
-  digitalWrite(M1, LOW);
-  analogWrite(E2, 120);
-  digitalWrite(M2, HIGH);
+  analogWrite(E1, 0);
+  analogWrite(E2, 0);
   delay(60);
-
-  stopCoast();
 }
 
-//ultra
+// Ultrasonic measurement
 long getDistanceCm() {
+
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
+
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
   unsigned long duration = pulseIn(echoPin, HIGH, 30000);
+
   if (duration == 0) return 0;
 
-  return (long)(duration * 0.0343 / 2.0);
+  return duration / 58.2;
 }
